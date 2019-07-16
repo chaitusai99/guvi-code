0190e5e16ff9eaf361a9bb8107c3e84361105f20
MALEMARPURAM CHAITANYA SAI <40909669+chaitusai99@users.noreply.github.com>
2019-07-16 22:24:34 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,6 +1,6 @@
-num = float(input("Enter a number: "))
-if num >= 0:
-   if num == 0:
+x = float(input("Enter a number: "))
+if x >= 0:
+   if x == 0:
        print("Zero")
    else:
        print("Positive number")
